1500c56994240e4c331057f1d2c2de6017ca2a38
Vraj535 <vraj.rajapara133360@marwadiuniversity.ac.in>
2025-11-10 13:14:47 +0530

    Add files via upload

---
+++ watches.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  for (let i = 0; i < slides.length; i++) slides[i].style.display = "none";
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // 4s
+}
+
+function plusSlides(n) {
+  slideIndex += n - 1;
+  showSlides();
+}
